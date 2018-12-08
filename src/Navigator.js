c34910dc74f37ff388f8b4dc7f19e37225cf3c5f
unknown <tmjn0809gmail.com>
2018-12-09 01:18:02 +0200

    Add bootstrap to html file

--- src/Navigator.js
+++ src/Navigator.js
@@ -19,8 +19,11 @@ class Navigator extends Component {
       return (
         <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span className="navbar-toggler-icon"></span>
+        </button>
           <Link className="navbar-brand" to="/">The page</Link>
-          <div id='nav_Bar'>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
               <Link className="nav-link" to="/">Home</Link>
